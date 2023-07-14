5a0f7c55d70a60a58b8a5573d7e66ab594968570
FatimaZah23 <fatimazahrae.ammari@um5r.ac.ma>
2023-07-14 07:45:32 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Programming is like building a multilingual puzzle
+ * main - A function that prints a string via the puts function.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle ")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
