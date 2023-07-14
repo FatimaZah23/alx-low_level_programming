b923f001c086158f224bb75dcfbf6b85686fb465
FatimaZah23 <fatimazahrae.ammari@um5r.ac.ma>
2023-07-14 08:11:10 -0700

    wow

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - A C program that prints a quote without using printf or puts.
+ *
+ * Return: return 1 (failure)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
+	return (1);
+}
